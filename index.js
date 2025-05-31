3e4b6960c01ce5fea318d3967ec846d2f88a420b
Manuel <manuel.t.shade@gmail.com>
2025-05-31 20:46:11 +0100

    ping test

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-// Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+require('dotenv').config();
+// Get the bot token from environment variables
+const token = process.env.DISCORD_BOT_TOKEN;
+if (!token) {
+	console.error('Please set the DISCORD_BOT_TOKEN environment variable in your .env file.');
+	process.exit(1);
+}
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -10,6 +15,13 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+
+client.on('message', msg => {
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+	}
 });
 
 // Log in to Discord with your client's token
